34a09a786850e9e3fe9de10a575c47e44af819d2
Pablo Garcia <pablo.garcia.saleta@gmail.com>
2018-04-09 17:36:35 +0200

    before adding to table

--- js/draw.js
+++ js/draw.js
@@ -1,119 +1,98 @@
-function validateForm() {
-    var x = document.getElementById("nombre");
-    var y = document.getElementById("tdMail");
-    var name = x.value;
-    var mail = y.value;
-    if (name == ""){
-        alert("El campo 'Nombre' debe de estar cubierto");
-        return false;
-    }
-    if (mail == ""){
-        alert("El campo 'Correo' debe de estar cubierto");
-        return false;
-    }
-} 
-
+        // FUNCION PARA AÑADIR LOS DATOS INTRODUCIDOS EN EL FORM AL LOCAL STORAGE DEL BROWSER
 function addToLocal() {
-    var x = document.getElementById("nombre");
-    var y = document.getElementById("tdMail");
-    var name = x.value;
-    var mail = y.value;
 	if (typeof(Storage) !== "undefined") {
-        // Store
-        localStorage.setItem("name", name);
-        localStorage.setItem("mail", mail);
-        //retrieve
-        document.getElementById("tdName").innerHTML = localStorage.getItem("name");
-        document.getElementById("tdMail").innerHTML = localStorage.getItem("mail");
+        // new arrays
+        var aNames = [];
+        // get input text
+        var txtName = document.getElementById("txtName");
+        var txtEmail = document.getElementById("txtEmail");
+        var nombre = txtName.value;
+        var email = txtEmail.value;
+
+        // get array from local and its data or create array if not exist
+        var retrievedUser = localStorage.getItem("aNames");
+        if ((nombre!=="") && (email!=="")) {
+            if (retrievedUser == null){
+                aNames.push({"nombre": nombre, "email": email});
+                localStorage.setItem('aNames', JSON.stringify(aNames));
+                AddRow(nombre, email);
+            }else {
+                AddRow(nombre, email);
+                // Parse the serialized data back into an array of objects
+                aNames = JSON.parse(retrievedUser);
+                // Push the new data (whether it be an object or anything else) onto the array
+                aNames.push({"nombre": nombre, "email": email});
+                // Re-serialize the array back into a string and store it in localStorage
+                localStorage.setItem('aNames', JSON.stringify(aNames));
+                // Add data to table
+            }
+        } else {
+                alert('Completa los campos nombre e email.');
+        }        
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
 }
 
-function refreshTable(){
-    /*var table = document.getElementById("tablaParticipantes");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = name;
-    cell2.innerHTML = mail;
-    cell3.innerHTML = '<img src="../img/delete.png" alt="delete" title="delete" height="50" width="50" onclick="deleteFromTable()">';*/
-
-    var tabla=document.getElementById("tablaParticipantes");
-    var tr =document.createElement("tr");
-    var td =document.createElement("td");
-    var td2 =document.createElement("td");
-    var td3 =document.createElement("td");
-
-    td.setAttribute("id", "tdName");
-
-    td2.setAttribute("id", "tdMail");
-
-    td3.setAttribute("onmouseenter", "transformToDele()");
-    td3.setAttribute("onmouseleave", "transformToTick()");
-    td3.setAttribute("onclick", "deleteRow(this)");
-    td3.setAttribute("id", "imgtd");
-
-    var tick = document.createElement("img");
-    tick.setAttribute("src", "../img/tick.png");
-    tick.setAttribute("height", "50");
-    tick.setAttribute("width", "50");
-    tick.setAttribute("alt", "Delete");
-    tick.setAttribute("id", "tick");
-    td3.appendChild(tick);
-
-    // Retrieve from localStorage
-    localname = document.getElementById("tdName").innerHTML = localStorage.getItem("name");
-    localmail = document.getElementById("tdMail").innerHTML = localStorage.getItem("mail");
-
-    td.appendChild(localname);
-    td2.appendChild(localmail);
-
-    tr.appendChild(td);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tabla.appendChild(tr); 
-}
-
-function deleteRow(r) {
-    var i = r.parentNode.rowIndex;
-    document.getElementById("tablaParticipantes").deleteRow(i);
-}
-
+        // FUNCION PARA SACAR MENSAJES POR LA CONSOLA
 function showMessage(){
     var text = " ->OpositaTest: Iniciando sorteo de la pizarra...\n->OpositaTest: ";
     var area=document.getElementById("out").value = text;
 }
 
-function transformToDele(){
-    var tick = document.getElementById("tick");
-    var pos = document.getElementById("imgtd");
+        // FUNCION PARA ELIMINAR FILA CUANDO CLICKEAS IMAGEN
 
-    pos.removeChild(tick);
+function Remove(img) {
+    //Determine the reference of the Row using the Button.
+    var row = img.parentNode.parentNode;
+    var name = row.getElementsByTagName("TD")[0].innerHTML;
+    //Get the reference of the Table.
+    var table = document.getElementById("tablaParticipantes");
 
-    var dele = document.createElement("img");
-    dele.setAttribute("src", "../img/delete.png");
-    dele.setAttribute("height", "50");
-    dele.setAttribute("width", "50");
-    dele.setAttribute("alt", "Delete");
-    dele.setAttribute("id", "dele");
-    pos.appendChild(dele);
+    //Delete the Table row using it's Index.
+    table.deleteRow(row.rowIndex);
 }
 
-function transformToTick(){
-    var pos = document.getElementById("imgtd");
-    var elim = document.getElementById("dele");
+    //  FUNCION PARA AÑADIR A LA TABLA LA FILA CON LOS DATOS INTRODUCIDOS
 
-    pos.removeChild(elim);
+function AddRow(name, email) {
+    //Get the reference of the Table's TBODY element.
+    var tBody = document.getElementById("tablaParticipantes").getElementsByTagName("TBODY")[0];
+
+    //Add Row.
+    var row = tBody.insertRow(-1);
+
+    //Add Name cell.
+    var cell = row.insertCell(-1);
+    cell.innerHTML = name;
+
+    //Add email cell.
+    cell = row.insertCell(-1);
+    cell.innerHTML = email;
+
+    //Add img cell.
+    cell = row.insertCell(-1);
 
     var tick = document.createElement("img");
     tick.setAttribute("src", "../img/tick.png");
     tick.setAttribute("height", "50");
     tick.setAttribute("width", "50");
-    tick.setAttribute("alt", "Delete");
-    tick.setAttribute("id", "tick");
-    pos.appendChild(tick);
+    tick.setAttribute("onclick", "Remove(this);");
+    tick.setAttribute("onmouseenter", "transformToTick(this)");
+    tick.setAttribute("onmouseleave", "transformToDele(this)");
+    tick.value = "Remove";
+    tick.type = "img";
+    cell.appendChild(tick);
+}
 
+        // FUNCIONES ARA TRANSFORMAR LA IMAGEN AL PASAR POR ENCIMA
 
+function transformToDele(img){
+    //document.getElementsByTagName("img").src= "../img/tick.png";
+    img.setAttribute("src", "../img/tick.png");
+}
+
+function transformToTick(img){
+    //document.getElementsByTagName("img").src= "../img/delete.png";
+    img.setAttribute("src", "../img/delete.png");
 }
