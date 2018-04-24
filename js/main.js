2a86990fded4e6b7d299c4b3e4e0dfc3ac9b2b27
Pablo Garcia <pablo.garcia.saleta@gmail.com>
2018-04-24 12:26:41 +0200

    AÑADIENDO ENCUESTA

---
+++ js/main.js
@@ -0,0 +1,580 @@
+var encuestas = 
+[
+	{
+		"id": 1, 
+		"titulo": "¿Qué personaje de los simpsons serías?", 
+		"preguntas":
+			[
+				{
+					"id_pregunta": 1, 
+					"imagen": "https://desempleotecnologico.files.wordpress.com/2013/07/work-relax.jpeg?w=500",
+					"enunciado_pregunta": "¿Qué te gusta más?", 
+					"respuestas": 
+						[
+							{
+								"id_respuesta": 1, 
+								"enunciado_respuesta": "Comer donuts.", 
+								"valor": 4 
+							},
+		 					{
+		 						"id_respuesta": 2,
+		 						"enunciado_respuesta": "Estudiar.",
+		 						"valor": 1
+		 					},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "Hacer gamberradas.", 
+								"valor": 2
+							},
+							{
+								"id_respuesta": 4, 
+								"enunciado_respuesta": "Servir copas.", 
+								"valor": 6
+							}
+						]
+				},  /*1ª pregunta, 1ª encuesta*/
+
+	 			{
+	 				"id_pregunta": 2, 
+	 				"imagen": "http://www.quieroimagenes.com/i/Homer-trabajando-imagen.jpg",
+	 				"enunciado_pregunta": "¿Te gusta trabajar?", 
+	 				"respuestas":
+	 					[
+	 						{
+		 						"id_respuesta": 1, 
+		 						"enunciado_respuesta": "Sí, mucho.", 
+		 						"valor": 1
+		 					},
+							{
+								"id_respuesta": 2, 
+								"enunciado_respuesta": "No, nada.", 
+								"valor": 5
+							},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "No mucho.", 
+								"valor": 6
+							}
+	 					]
+				},  /*2ª pregunta, 1ª encuesta*/
+
+				{
+					"id_pregunta": 3, 
+					"imagen": "https://unaodoscopas.files.wordpress.com/2016/07/o_marcalpena_moes.jpg"	,
+					"enunciado_pregunta": "¿Qué preferirías hacer?", 
+					"respuestas": 
+						[
+							{
+								"id_respuesta": 1, 
+								"enunciado_respuesta": "Comer, beber y dormir.", 
+								"valor": 4
+							},
+							{
+								"id_respuesta": 2, 
+								"enunciado_respuesta": "Vacilar.", 
+								"valor": 2
+							},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "Tocar el saxofón.", 
+								"valor": 1
+							},
+							{
+								"id_respuesta": 4, 
+								"enunciado_respuesta": "Reirse de borrachos.", 
+								"valor": 7
+							}
+						]
+				}
+			],	
+		"soluciones": 
+			[
+				{
+					"id_solucion": 1,
+					"solucion": "Lisa Simpson",
+					"imagen": "http://images.yodibujo.es/_uploads/_tiny_galerie/20130414/lisa-simpson-hija_6zs.jpg",
+					"explicacion": "Eres una persona irresponsable, infantil, lenta y torpe. Te encanta la comida, e ir al bar a beber birras. Pero algo bueno tienes, eres muy agradable, gracioso y fiel.",
+					"rango": [1,5]
+				},
+				{
+					"id_solucion": 1,
+					"solucion": "Bart Simpson",
+					"imagen": "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+					"explicacion": "Eres una persona irresponsable, infantil, lenta y torpe. Te encanta la comida, e ir al bar a beber birras. Pero algo bueno tienes, eres muy agradable, gracioso y fiel.",
+					"rango": [6,10]
+				},
+				{
+					"id_solucion": 1,
+					"solucion": "Homer Simpson",
+					"imagen": "http://interactive.nydailynews.com/2016/05/simpsons-quiz/img/simp1.jpg",
+					"explicacion": "Eres una persona irresponsable, infantil, lenta y torpe. Te encanta la comida, e ir al bar a beber birras. Pero algo bueno tienes, eres muy agradable, gracioso y fiel.",
+					"rango": [11,15]
+				},
+				{
+					"id_solucion": 1,
+					"solucion": "Moe Szyslak",
+					"imagen": "http://s03.s3c.es/imag/_v0/640x300/1/1/c/moe.jpg",
+					"explicacion": "Eres una persona irresponsable, infantil, lenta y torpe. Te encanta la comida, e ir al bar a beber birras. Pero algo bueno tienes, eres muy agradable, gracioso y fiel.",
+					"rango": [16,20]
+				}
+			]
+	},
+
+	{
+		"id": 2, 
+		"titulo": "¿Qué personaje de los Simpsons serías?", 
+		"preguntas":
+			[
+				{
+					"id_pregunta": 1, 
+					"imagen": "https://vignette.wikia.nocookie.net/inciclopedia/images/d/d7/Lisa_simpson_1.gif/revision/latest?cb=20090202033921",
+					"enunciado_pregunta": "¿Qué te gusta más?", 
+					"respuestas": 
+						[
+							{
+								"id_respuesta": 1, 
+								"enunciado_respuesta": "Comer donuts.", 
+								"valor": 4 
+							},
+		 					{
+		 						"id_respuesta": 2,
+		 						"enunciado_respuesta": "Estudiar.",
+		 						"valor": 1
+		 					},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "Hacer gamberradas.", 
+								"valor": 2
+							},
+							{
+								"id_respuesta": 4, 
+								"enunciado_respuesta": "Servir copas.", 
+								"valor": 6
+							}
+						]
+				},  /*1ª pregunta, 1ª encuesta*/
+
+	 			{
+	 				"id_pregunta": 2, 
+	 				"imagen": "http://www.puzzlesonline.es/puzzles/2015/simpsons/tirachinasdebart.jpg",
+	 				"enunciado_pregunta": "¿Te gusta trabajar?", 
+	 				"respuestas":
+	 					[
+	 						{
+		 						"id_respuesta": 1, 
+		 						"enunciado_respuesta": "Sí, mucho.", 
+		 						"valor": 1
+		 					},
+							{
+								"id_respuesta": 2, 
+								"enunciado_respuesta": "No, nada.", 
+								"valor": 5
+							},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "No mucho.", 
+								"valor": 6
+							}
+	 					]
+				},  /*2ª pregunta, 1ª encuesta*/
+
+				{
+					"id_pregunta": 3, 
+					"imagen": "http://www.freakingnews.com/pictures/37000/Homer-Asleep-on-a-Sofa-37122.jpg",
+					"enunciado_pregunta": "¿Qué preferirías hacer?", 
+					"respuestas": 
+						[
+							{
+								"id_respuesta": 1, 
+								"enunciado_respuesta": "Comer, beber y dormir.", 
+								"valor": 4
+							},
+							{
+								"id_respuesta": 2, 
+								"enunciado_respuesta": "Vacilar.", 
+								"valor": 2
+							},
+							{
+								"id_respuesta": 3, 
+								"enunciado_respuesta": "Tocar el saxofón.", 
+								"valor": 1
+							},
+							{
+								"id_respuesta": 4, 
+								"enunciado_respuesta": "Reirse de borrachos.", 
+								"valor": 7
+							}
+						]
+				}
+			]
+	},
+
+];
+
+var comment = [
+	{
+		"id_comment": 1,
+		"texto": "¡Hola! Este es un comentario prueba de un usuario anónimo."
+	},
+	{
+		"id_comment": 2,
+		"texto": "Por el momento solo habrá comentarios anónimos, pero es muy posible que se añadan usuarios en futuras versiones."
+	},
+	{
+		"id_comment": 3,
+		"texto": "Por ahora tampoco se pude responder a un comentario directamente. También se añadirá dicha funcionalidad."
+	}
+];
+
+var puntuacion = 0;
+var Id_encuesta = 1;
+var PPos = 0;
+var key = null;
+var pos = 0;
+var printed = 0;
+
+
+
+
+// FUNCIÓN ONLOAD QUE INICIALIZA VARIAS FUNCIONES
+function cargar(){
+	$(window).on('scroll',function(){
+	    navbar();	
+	});
+	imprimirEncuesta(getEPos());
+	imprimirSavedComments();
+}
+
+//FUNCION PARA MODIFICAR EL NAVBAR
+function navbar(){
+	var encuesta = $('#encuesta').offset().top;
+	var stop = Math.round($(window).scrollTop());
+	if (key) {
+    	if (stop >= encuesta) {
+	    	$("#nav").removeClass("navbar-light");
+	        $('#nav').addClass('navbar-dark');
+	        $('#nav').css("background-color", "black");
+	        $("#nav").animate({height: "60px"});
+	        key = false;
+    	}
+    } else if (!key){
+    	if (stop < encuesta) {
+	    	$("#nav").removeClass("navbar-dark");
+	        $('#nav').css("background-color", "#EDDBF3");
+	        $('#nav').addClass('navbar-light');
+	        $("#nav").animate({height: "100px"});
+	        key = true;
+	    }
+    }  
+
+    if (key == null){
+    	if (stop >= encuesta) {
+	    	$("#nav").removeClass("navbar-light");
+	        $('#nav').addClass('navbar-dark');
+	        $('#nav').css("background-color", "black");
+	        $("#nav").css("height", "60px");
+	        key = false;
+    	} else if (stop < encuesta) {
+	    	$("#nav").removeClass("navbar-dark");
+	        $('#nav').css("background-color", "#EDDBF3");
+	        $('#nav').addClass('navbar-light');
+	        $("#nav").css("height", "100px");
+	        key = true;
+	    }
+    }  	
+}
+
+// FUNCION QUE SACA LAS PREGUNTAS Y RESPUESTAS
+function imprimirEncuesta(id_encuesta){
+	for (var i = 0; i < encuestas.length; i++){
+		if (id_encuesta === encuestas[i].id) {
+			this.pos = i;
+		}
+	}
+		//datos encuesta
+	var id_encuesta = encuestas[pos].id;
+	var titulo = encuestas[pos].titulo;
+	// create titulo de la encuesta
+	var h1 = document.createElement("h1");
+	h1.setAttribute("id", "titulo_encuesta_h1");
+	h1.innerHTML = titulo;
+	//$("#titulo_encuesta").append(h1);
+	$(h1).hide().appendTo("#encuesta").fadeIn(1000);
+	imprimirPreguntas(getEPos(), this.pos, getPPos());	
+}
+
+function imprimirPreguntas(id_encuesta, pos, pos_pregunta){
+		//datos pregunta
+	var id_pregunta = encuestas[pos].preguntas[pos_pregunta].id_pregunta;
+	var enunciado_pregunta = encuestas[pos].preguntas[pos_pregunta].enunciado_pregunta;
+	var ruta_img = encuestas[pos].preguntas[pos_pregunta].imagen;
+
+	// create imagen de la pregunta
+	var img = document.createElement("img");
+	img.setAttribute("src", ruta_img);
+	img.setAttribute("class", "img-fluid rounded");
+	$(img).hide().appendTo("#img_encuesta").fadeIn(1000);
+	//$("#img_encuesta").append(img);
+
+	// create enunciado de la pregunta
+	var h4 = document.createElement("h4");
+	h4.setAttribute("id", "enunciado_pregunta_h4");
+	h4.innerHTML = enunciado_pregunta;
+	$(h4).hide().appendTo("#pregunta").fadeIn(1000);
+	//$("#pregunta").append(h4);
+
+	// create respuestas de la pregunta
+	for (var j = 0; j < encuestas[pos].preguntas[pos_pregunta].respuestas.length; j++){
+		//datos respuesta
+		var id_respuesta = encuestas[pos].preguntas[pos_pregunta].respuestas[j].id_respuesta;
+		var enunciado_respuesta = encuestas[pos].preguntas[pos_pregunta].respuestas[j].enunciado_respuesta; 
+		
+
+		var div = document.createElement("div");
+	    div.setAttribute("class", "col-12 justify-content-center text-center");
+	    div.setAttribute("style", "padding-bottom:20px");
+
+		var respuesta = document.createElement("input");
+	    respuesta.setAttribute("type", "button");
+	    respuesta.setAttribute("id", id_respuesta);
+	    respuesta.setAttribute("class", "btn btn-dark btn-width");
+	    respuesta.setAttribute("value", enunciado_respuesta);
+	    respuesta.setAttribute("data-idResponse", id_respuesta);
+	    respuesta.setAttribute("data-idQuestion", id_pregunta);
+	    respuesta.setAttribute("data-idEncuesta", id_encuesta);
+	    respuesta.setAttribute("onclick", "accion(this)");
+
+	    $(respuesta).hide().appendTo(div).fadeIn(1000);
+    	//div.append(respuesta);
+	   
+		//$("#div_respuestas").append(div);
+		$(div).hide().appendTo("#div_respuestas").fadeIn(1000);
+		setPPos(pos_pregunta + 1);
+		//progress bar
+		var num_preg = encuestas[pos].preguntas.length;
+		var newWidth = id_pregunta / num_preg * 100;
+		progress(newWidth);
+	}
+	this.printed ++;
+}
+
+// funcion que imprime el resultado
+
+function imprimirSolucion(pos){
+	
+	//$("#titulo_encuesta").append(h1);
+
+	for (var i = 0; i < encuestas[pos].soluciones.length; i++){
+		console.log(encuestas[pos].soluciones[i].rango[0]);
+		if ((encuestas[pos].soluciones[i].rango[0]) <= (this.puntuacion) && (this.puntuacion) <= (encuestas[pos].soluciones[i].rango[1])){
+			var solucion = encuestas[pos].soluciones[i].solucion;
+			var ruta = encuestas[pos].soluciones[i].imagen;
+			var explicacion = encuestas[pos].soluciones[i].explicacion;
+
+			var h1 = document.createElement("h1");
+			h1.innerHTML = solucion;
+			$(h1).hide().appendTo("#titulo_encuesta").fadeIn(1000);
+
+			var img = document.createElement("img");
+			img.setAttribute("src", ruta);
+			$(img).hide().appendTo("#img_encuesta").fadeIn(1000);
+
+			var result = document.createElement("p");
+			result.innerHTML = explicacion;
+			$(result).hide().appendTo("#pregunta").fadeIn(1000);
+		}
+	}
+}
+
+// funcion que realizan los botones de las respuestas
+function accion(boton){
+	var id = boton.id;
+	var selfEncuesta;
+	var selfPregunta;
+	var selfRespuesta;
+	var selfValor;
+	for (var i = 0; i < encuestas.length; i++){
+		if ($(boton).attr("data-idEncuesta") == encuestas[i].id){
+			selfEncuesta = encuestas[i];
+			for (var j = 0; j < encuestas[i].preguntas.length; j++) {
+				if ($(boton).attr("data-idQuestion") == encuestas[i].preguntas[j].id_pregunta) {
+					selfPregunta =  encuestas[i].preguntas[j];
+				}
+			}
+		}
+	}
+	
+	for (var h = 0; h < selfPregunta.respuestas.length; h++){
+		if ($(boton).attr("data-idResponse") == selfPregunta.respuestas[h].id_respuesta) {
+			selfRespuesta = selfPregunta.respuestas[h];
+		}
+	}
+
+	selfValor = selfRespuesta.valor;
+	setPuntuacion(selfValor);
+	limpiarSalida();
+	// si el numero de preguntas impresas es igual al total de preguntas de esa encuesta, sacamos el resultado
+	if (this.printed == encuestas[this.pos].preguntas.length){
+		imprimirSolucion(this.pos);
+	} else {
+		imprimirPreguntas(getEPos(), this.pos, getPPos());	
+	}
+}
+
+// funcion que resetea la pregunta y los botones
+function limpiarSalida(){
+	$("#titulo_encuesta").empty();
+	$("#img_encuesta").empty();
+	$("#pregunta").empty();
+	$("#div_respuestas").empty();
+}
+
+// funcion que saca por pantalla los comentarios ya guardados
+function imprimirSavedComments(){
+	for (var i = 0; i < comment.length; i++){
+		// creo div row
+			var div = document.createElement("div");
+			div.setAttribute("class", "row");
+			div.setAttribute("style", "margin-bottom: 25px");
+
+				// creo div col 1
+				var div1 = document.createElement("div");
+		    	div1.setAttribute("class", "col-sm-2 text-center");
+
+		    		// creo img de div1
+		    		var img = document.createElement("img");
+				    img.setAttribute("src", "https://png.icons8.com/metro/1600/gender-neutral-user.png");
+				    img.setAttribute("class", "img-circle");
+				    img.setAttribute("height", "65");
+				    img.setAttribute("width", "65");
+				    img.setAttribute("alt", "avatar");
+
+				// meto img en div1
+				$(img).hide().appendTo(div1).fadeIn(1000);
+		    	//div1.append(img);
+
+		    	// creo div col 2
+			    var div2 = document.createElement("div");
+			    div2.setAttribute("class", "col-sm-10");
+			    div2.setAttribute("style", "margin-bottom: 10px");
+
+			    	// creo p de div2
+				    var p = document.createElement("p");
+				    p.innerHTML = comment[comment.length-1].texto;
+			    // meto p en div 2
+			    //div2.append(p);
+			    $(p).hide().appendTo(div2).fadeIn(1000);
+		    // meto div1 y div2 en div row
+		    $(div1).hide().appendTo(div).fadeIn(1000);
+		    $(div2).hide().appendTo(div).fadeIn(1000);
+		    //div.append(div1);
+		    //div.append(div2);
+		    //meto div row en div row rowComment
+		    $(div).hide().appendTo("#rowComment").fadeIn(1000);
+		    //$("#rowComment").append(div);
+		    $("#coment").val("");
+	}
+}
+// funcion que imprime los comentarios nuevos y los guarda (temporalmente)
+function imprimirComentarios(){
+	var texto = $("#coment").val();
+	if (texto != "") {
+		size = comment.length;
+		var prevId = comment[size - 1].id_comment;
+		var newId = prevId + 1;
+		comment.push({"id_comment": newId, "texto": texto});
+		if (comment[size].texto != ""){
+			// creo div row
+			var div = document.createElement("div");
+			div.setAttribute("class", "row");
+			div.setAttribute("style", "margin-bottom: 25px");
+
+				// creo div col 1
+				var div1 = document.createElement("div");
+		    	div1.setAttribute("class", "col-sm-2 text-center");
+
+		    		// creo img de div1
+		    		var img = document.createElement("img");
+				    img.setAttribute("src", "https://png.icons8.com/metro/1600/gender-neutral-user.png");
+				    img.setAttribute("class", "img-circle");
+				    img.setAttribute("height", "65");
+				    img.setAttribute("width", "65");
+				    img.setAttribute("alt", "avatar");
+
+				// meto img en div1
+				$(img).hide().appendTo(div1).fadeIn(1000);
+		    	//div1.append(img);
+
+		    	// creo div col 2
+			    var div2 = document.createElement("div");
+			    div2.setAttribute("class", "col-sm-10");
+			    div2.setAttribute("style", "margin-bottom: 10px");
+
+			    	// creo p de div2
+				    var p = document.createElement("p");
+				    p.innerHTML = comment[size].texto;
+			    // meto p en div 2
+			    //div2.append(p);
+			    $(p).hide().appendTo(div2).fadeIn(1000);
+		    // meto div1 y div2 en div row
+		    $(div1).hide().appendTo(div).fadeIn(1000);
+		    $(div2).hide().appendTo(div).fadeIn(1000);
+		    //div.append(div1);
+		    //div.append(div2);
+		    //meto div row en div row rowComment
+		    $(div).hide().appendTo("#rowComment").fadeIn(1000);
+		    //$("#rowComment").append(div);
+		    $("#coment").val("");
+		}
+	} else {
+		/*var alerta = document.createElement("div");
+		alerta.setAttribute("class", "alert alert-success");
+		alerta.innerHTML = "strong>Introduzca texto en la casilla de comentarios.</strong>";
+
+		var a = document.createElement("a");
+		a.setAttribute("href", "#");
+		a.setAttribute("class", "close");
+		a.setAttribute("data-dismiss", "alert");
+		a.setAttribute("aria-label", "close");
+		a.innerHTML = "&times;" ;
+
+		alerta.append(a);*/
+	}	
+}
+
+//funcion que elimina la salida de los comentarios
+function limpiarComentarios(){
+	$("#rowComment").empty();
+}
+
+// funcion que actualiza la barra de progreso
+function progress(newWidth){
+	$("#PBar").css({width: newWidth + "%"});
+	$("#PBar").val(newWidth + "%");
+}
+
+
+// GETTERS Y SETTERS
+function setEPos(EPos){
+	this.Id_encuesta = Id_encuesta;
+}
+
+function getEPos(){
+	return this.Id_encuesta;
+}
+
+function setPPos(PPos){
+	this.PPos = PPos;
+}
+
+function getPPos(){
+	return this.PPos;
+}
+
+function setPuntuacion(nueva_puntuacion){
+	this.puntuacion += nueva_puntuacion;
+}
+
+function getPuntuacion(){
+	return this.puntuacion;
+}
