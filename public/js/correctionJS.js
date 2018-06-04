cb1f8cdbf3a379fff748502b377ea4fe6e506cd2
Pablo Garcia <pablo.garcia.saleta@gmail.com>
2018-06-04 12:58:36 +0200

    terminado

--- public/js/correctionJS.js
+++ public/js/correctionJS.js
@@ -1,48 +1,48 @@
 opos =
     [
         {
-            "value": "jueces_fiscales",
+            "value": "2",
             "puntuacion": [10, -0.33, 0]
         },
 
         {
-            "value": "justicia",
+            "value": "4",
             "puntuacion": [20, -0.33, 0]
         },
 
         {
-            "value": "interior",
+            "value": "5",
             "puntuacion": [30, -0.33, 0]
         },
 
         {
-            "value": "hacienda",
+            "value": "6",
             "puntuacion": [40, -0.33, 0]
         },
 
         {
-            "value": "administracion_general",
+            "value": "7",
             "puntuacion": [50, -0.33, 0]
         },
 
         {
-            "value": "fuerzas_y_cuerpos_seguridad",
+            "value": "8",
             "puntuacion": [60, -0.33, 0]
         },
 
         {
-            "value": "sanidad",
+            "value": "10",
             "puntuacion": [70, -0.33, 0]
         },
 
         {
-            "value": "legislacion",
+            "value": "11",
             "puntuacion": [80, -0.33, 0]
         },
 
         {
-            "value": "abogacia",
-            "puntuacion": [90, -0.33, 0]
+            "value": "0",
+            "puntuacion": [0, 0, 0]
         }
     ];
 scoreCorrectQuestions = $("[data-js='scoreCorrectQuestions']");
@@ -54,7 +54,7 @@ nincorrectas = $("[data-js='numberIncorrectQuestions']");
 nblanco = $("[data-js='numberBlankQuestions']");
 ntotal = $("[data-js='totalQuestions']");
 
-selection = $("[data-js='changeOpoSelect']");
+selection = $("[data-js='changeOppostionSelect']");
 
 nota = $("[data-js='toPlaceMark']");
 notaCol = $("[data-js='markCol']");
@@ -65,12 +65,12 @@ btnCorrect = $("[data-js='corregir']");
 $(document).ready(function(){
     clear();
     startEvents();
-    // changeOpos();
+    changeOpos();
     }
 );
 
 function clear() {
-    // $("[data-js='changeOpoSelect']").val("Personalizado");
+    selection.val("0");
     ncorrectas.val("0");
     nincorrectas.val("0");
     nblanco.val("0");
@@ -78,41 +78,22 @@ function clear() {
     scoreIncorrectQuestions.val("0");
     scoreBlankQuestions.val("0");
     ntotal.val("0");
-    nota.val("0");
+    nota.val("");
 }
 
 function changeOpos() {
     for( i=0; i < opos.length; i++){
         if (selection.val() === opos[i]["value"]){
-
-            scoreCorrectQuestions.prop('disabled', true);
-            scoreIncorrectQuestions.prop('disabled', true);
-            scoreBlankQuestions.prop('disabled', true);
             scoreCorrectQuestions.val(opos[i]["puntuacion"][0]);
             scoreIncorrectQuestions.val(opos[i]["puntuacion"][1]);
             scoreBlankQuestions.val(opos[i]["puntuacion"][2]);
-
-        } else if(selection.val() === "personalizado") {
-
-            scoreCorrectQuestions.prop('disabled', false);
-            scoreIncorrectQuestions.prop('disabled', false);
-            scoreBlankQuestions.prop('disabled', false);
-            scoreCorrectQuestions.val("0");
-            scoreIncorrectQuestions.val("0");
-            scoreBlankQuestions.val("0");
         }
     }
 }
 
 function corregir(){
-    if ((parseInt(ncorrectas.val()) > 0) && (parseInt(nincorrectas.val()) > 0) && (parseInt(nblanco.val()) > 0)) {
+    if ((parseInt(ncorrectas.val()) >= 0) && (parseInt(nincorrectas.val()) >= 0) && (parseInt(nblanco.val()) >= 0)) {
         total =  parseInt(ncorrectas.val()) + parseInt(nincorrectas.val()) + parseInt(nblanco.val());
-
-        if ((scoreCorrectQuestions.val() === "0") || (scoreIncorrectQuestions.val() === "0")) {
-            alert("Establezca un sistema de evaluación válido.");
-        } else if (total === "0") {
-            alert("Introduzca una cantidad superior a 0 de preguntas");
-        } else {
             totalCorrectas = parseFloat(scoreCorrectQuestions.val()) * parseInt(ncorrectas.val());
             totalIncorrectas = parseFloat(scoreIncorrectQuestions.val()) * parseInt(nincorrectas.val());
             totalBlanco = parseFloat(scoreBlankQuestions.val()) * parseInt(nblanco.val());
@@ -120,8 +101,6 @@ function corregir(){
             calculoNota = totalBlanco + totalCorrectas + totalIncorrectas;
 
             nota.text("Su nota es de " + calculoNota + " sobre " + total * scoreCorrectQuestions.val());
-            notaCol.addClass("colored");
-        }
     } else {
         alert("No puede haber un número negativo de preguntas.");
     }
@@ -172,6 +151,28 @@ function clearInput(focus) {
 }
 
 function startEvents() {
+    // focus events
+    scoreCorrectQuestions.on( "focus", function( event ) {
+        clearInput(scoreCorrectQuestions);
+        setPersonalizado();
+    });
+    scoreIncorrectQuestions.on( "focus", function( event ) {
+        clearInput(scoreIncorrectQuestions);
+        setPersonalizado();
+    });
+    scoreBlankQuestions.on( "focus", function( event ) {
+        clearInput(scoreBlankQuestions);
+        setPersonalizado();
+    });
+    ncorrectas.on( "focus", function( event ) {
+        clearInput(ncorrectas);
+    });
+    nincorrectas.on( "focus", function( event ) {
+        clearInput(nincorrectas);
+    });
+    nblanco.on( "focus", function( event ) {
+        clearInput(nblanco);
+    });
     // focusout events
     scoreCorrectQuestions.on( "focusout", function( event ) {
         puntuacionEvent();
@@ -194,6 +195,15 @@ function startEvents() {
     });
 
     // keypress events
+    scoreCorrectQuestions.on( "keypress", function( event ) {
+        return justNumbers(event);
+    });
+    scoreIncorrectQuestions.on( "keypress", function( event ) {
+        return justNumbers(event);
+    });
+    scoreBlankQuestions.on( "keypress", function( event ) {
+        return justNumbers(event);
+    });
     ncorrectas.on( "keypress", function( event ) {
         return justNumbers(event);
     });
@@ -212,25 +222,19 @@ function startEvents() {
         changeOpos();
     });
 
+}
 
-    // focus events
-    scoreCorrectQuestions.on( "focus", function( event ) {
-        clearInput(scoreCorrectQuestions);
-    });
-    scoreIncorrectQuestions.on( "focus", function( event ) {
-        clearInput(scoreIncorrectQuestions);
-    });
-    scoreBlankQuestions.on( "focus", function( event ) {
-        clearInput(scoreBlankQuestions);
-    });
-    ncorrectas.on( "focus", function( event ) {
-        clearInput(ncorrectas);
-    });
-    nincorrectas.on( "focus", function( event ) {
-        clearInput(nincorrectas);
-    });
-    nblanco.on( "focus", function( event ) {
-        clearInput(nblanco);
-    });
+function setPersonalizado() {
+    scoreCorrectVal = getInputVal(scoreCorrectQuestions);
+    scoreIncorrectVal = getInputVal(scoreIncorrectQuestions);
+    scoreBlankVal = getInputVal(scoreBlankQuestions);
+    selection.val("0");
+    scoreCorrectQuestions.val(scoreCorrectVal);
+    scoreIncorrectQuestions.val(scoreIncorrectVal);
+    scoreBlankQuestions.val(scoreBlankVal);
+}
+
+function getInputVal(input) {
+    return input.val();
 }
 
